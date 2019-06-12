9ac5b27e094f96a374d749ad548f3538226e7934
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2019-06-12 14:35:40 +0530

    Update fizz.go

--- fizz.go
+++ fizz.go
@@ -14,13 +14,31 @@ func buildURI(path string) string {
 	return "https://api.noopschallenge.com" + path
 }
 
-func main() {
+func postAnswer(answer string, questionURL string) string {
+	var data map[string]interface{}
 
+	requestBody, err := json.Marshal(map[string]string{
+		"answer": answer,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	postResp, postErr := http.Post(questionURL, "application/json", bytes.NewBuffer(requestBody))
+	if postErr != nil {
+		log.Fatalln(postErr)
+	}
+	defer postResp.Body.Close()
+	json.NewDecoder(postResp.Body).Decode(&data)
+
+	nextQuestionURL := data["nextQuestion"].(string)
+	return nextQuestionURL
+}
+
+func solve(questionURL string) string {
 	reader := bufio.NewReader(os.Stdin)
 
-	startURL := "/fizzbot/questions/1"
-
-	resp, err := http.Get(buildURI(startURL))
+	resp, err := http.Get(questionURL)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -31,61 +49,23 @@ func main() {
 	json.NewDecoder(resp.Body).Decode(&data)
 
 	fmt.Println(data["message"])
-
-	var answer string
-
-	answer, _ = reader.ReadString('\n')
-
-	requestBody, mErr := json.Marshal(map[string]string{
-		"answer": answer,
-	})
-
-	if mErr != nil {
-		log.Fatalln(mErr)
+	if data["numbers"] != nil {
+		fmt.Println(data["numbers"])
 	}
+	answer, _ := reader.ReadString('\n')
+	answer = answer[:len(answer)-1]
 
-	postResp, postErr := http.Post(buildURI(startURL), "application/json", bytes.NewBuffer(requestBody))
-	if postErr != nil {
-		log.Fatalln(postErr)
+	nextQuestionURL := postAnswer(answer, questionURL)
+
+	return nextQuestionURL
+}
+
+func main() {
+	fmt.Println("--------------------------------------------------------------------")
+	questionURL := "/fizzbot/questions/1"
+
+	for len(questionURL) > 0 {
+		questionURL = solve(buildURI(questionURL))
+		fmt.Println()
 	}
-	defer postResp.Body.Close()
-	json.NewDecoder(postResp.Body).Decode(&data)
-
-	for data["nextQuestion"] != nil {
-
-		tmpResp, tmpErr := http.Get(buildURI(data["nextQuestion"].(string)))
-		if tmpErr != nil {
-			log.Fatalln(tmpErr)
-		}
-		defer tmpResp.Body.Close()
-
-		json.NewDecoder(tmpResp.Body).Decode(&data)
-
-		fmt.Println(data)
-
-		var answer string //= "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz"
-
-		answer, _ = reader.ReadString('\n')
-		answer = answer[:len(answer)-1]
-
-		requestBody, mErr := json.Marshal(map[string]string{
-			"answer": answer,
-		})
-
-		if mErr != nil {
-			log.Fatalln(mErr)
-		}
-
-		fmt.Println(answer)
-
-		postResp, postErr := http.Post(buildURI(data["nextQuestion"].(string)), "application/json", bytes.NewBuffer(requestBody))
-		if postErr != nil {
-			log.Fatalln(postErr)
-		}
-		defer postResp.Body.Close()
-		json.NewDecoder(postResp.Body).Decode(&data)
-
-		fmt.Println(data)
-	}
-
 }
